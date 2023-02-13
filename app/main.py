4426b74c968ff481f2bb5ab582197d5c0a65890d
Liliana <soporte7@azsmart.net>
2023-02-13 16:20:05 -0500

    New docs

--- app/main.py
+++ app/main.py
@@ -1,21 +1,34 @@
 import utils
 import read_csv
 import charts
+import pandas as pd
+
 
 def run():
-  data = read_csv.read_csv('data.csv')
+
+  '''
   data = list(filter(lambda item : item['Continent'] == 'South America',data))
 
   countries = list(map(lambda x: x['Country'], data))
   percentages = list(map(lambda x: x['World Population Percentage'], data))
-  charts.generate_pie_chart(countries, percentages)
   
-  country = input('Type Country => ')
-  print(country)
+  '''
 
-  result = utils.population_by_country(data, country)
 
-  if len(result) > 0:
+df = pd.read_csv('data.csv')
+df = df[df['Continent'] == 'Africa']
+
+countries = df['Country'].values
+percentages = df['World Population Percentage'].values
+charts.generate_pie_chart(countries, percentages)
+
+data = read_csv.read_csv('data.csv')
+country = input('Type Country => ')
+print(country)
+
+result = utils.population_by_country(data, country)
+
+if len(result) > 0:
     country = result[0]
     print(country)
     labels, values = utils.get_population(country)
